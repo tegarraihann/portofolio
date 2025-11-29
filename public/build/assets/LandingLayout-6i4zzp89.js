cc62a9b0ae1acc558ab7c3dcbfeac8bce20daebc
tegarraihann <tegar.raihann01@gmail.com>
2025-11-29 14:55:23 +0700

    remove build dari gitignore

---
+++ public/build/assets/LandingLayout-6i4zzp89.js
@@ -0,0 +1,1 @@
+import{F as o}from"./Footer-CZ9iq9jR.js";import{c as t,o as a,d as e,A as r,b as c}from"./app-BUVwpb6_.js";const n={class:"min-h-screen bg-gray-50"},_={class:"flex-1"},i={class:"bg-gray-900 text-white"},f={__name:"LandingLayout",setup(d){return(s,l)=>(a(),t("div",n,[e("main",_,[r(s.$slots,"default")]),e("footer",i,[c(o)])]))}};export{f as _};
